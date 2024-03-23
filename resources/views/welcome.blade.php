1f04f827501d1ffd58369dc550c66d8098d499bd
Ashruni <ashrunirkjuly21@gmail.com>
2024-03-23 13:45:31 +0530

    FIRST COMMIT

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    @include('_header-post')
+    <x-feature/>
+    <div class="lg:grid lg:grid-cols-2">
+        <x-post-card />
+        <x-post-card />
+    </div>
+    <div class="lg:grid lg:grid-cols-3">
+        <x-post-card />
+        <x-post-card />
+        <x-post-card />
+    </div>
+</x-layout>
